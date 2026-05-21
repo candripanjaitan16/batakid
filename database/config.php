602b56a1dcbda2e4c0ca236256fd2ba93e5df197
candripanjaitan16 <candripanjaitan16@gmail.com>
2026-05-21 17:23:53 +0700

    Update database v0.1

--- database/config.php
+++ database/config.php
@@ -1,10 +1,14 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "batak_id";
+if (file_exists('config.php')) {
+    include 'config.php';
+} else {
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'batak_id');
+}
 
-$koneksi = mysqli_connect($host, $user, $pass, $db);
+$koneksi = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if (!$koneksi) {
     die("Koneksi database gagal: " . mysqli_connect_error());
